4675198c518f506cb739543a6d826c5819d8d9f0
Ivan Kasenka <kasenka.ivan@gmail.com>
2023-02-24 00:47:44 +0300

    feat: finish app

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.minValue = min;
+			this.maxValue = max;
     }
 
     guess() {
-
+      return this.guessValue = Math.ceil((this.maxValue + this.minValue) / 2);
     }
 
     lower() {
-
+      return this.maxValue = this.guessValue;
     }
 
     greater() {
-
+      return this.minValue = this.guessValue;
     }
 }
 
